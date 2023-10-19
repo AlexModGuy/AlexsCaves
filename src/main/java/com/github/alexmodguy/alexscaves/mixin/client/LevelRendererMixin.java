ffe1721930f45f43560a9d3070b597d7bdbcf9b2
Alexthe668 <alex.rowlands@cox.net>
2023-10-19 08:10:19 -0700

    fix #25

--- src/main/java/com/github/alexmodguy/alexscaves/mixin/client/LevelRendererMixin.java
+++ src/main/java/com/github/alexmodguy/alexscaves/mixin/client/LevelRendererMixin.java
@@ -3,19 +3,27 @@ package com.github.alexmodguy.alexscaves.mixin.client;
 import com.github.alexmodguy.alexscaves.AlexsCaves;
 import com.github.alexmodguy.alexscaves.server.block.EnergizedGalenaBlock;
 import com.github.alexmodguy.alexscaves.server.level.biome.ACBiomeRegistry;
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.*;
+import com.mojang.math.Axis;
 import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.client.renderer.DimensionSpecialEffects;
-import net.minecraft.client.renderer.LevelRenderer;
-import net.minecraft.client.renderer.ViewArea;
+import net.minecraft.client.renderer.*;
 import net.minecraft.client.renderer.culling.Frustum;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.SectionPos;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.LevelHeightAccessor;
 import net.minecraft.world.level.LightLayer;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.FogType;
+import net.minecraft.world.phys.Vec3;
+import org.joml.Matrix4f;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -40,6 +48,22 @@ public abstract class LevelRendererMixin {
     @Nullable
     private ViewArea viewArea;
 
+    @Shadow @Final private Minecraft minecraft;
+
+    @Shadow protected abstract void renderEndSky(PoseStack p_109781_);
+
+    @Shadow protected abstract boolean doesMobEffectBlockSky(Camera p_234311_);
+
+    @Shadow @Nullable private VertexBuffer skyBuffer;
+
+    @Shadow @Final private static ResourceLocation SUN_LOCATION;
+
+    @Shadow @Final private static ResourceLocation MOON_LOCATION;
+
+    @Shadow @Nullable private VertexBuffer starBuffer;
+
+    @Shadow @Nullable private VertexBuffer darkBuffer;
+
     @Inject(method = "Lnet/minecraft/client/renderer/LevelRenderer;setupRender(Lnet/minecraft/client/Camera;Lnet/minecraft/client/renderer/culling/Frustum;ZZ)V",
             at = @At(
                     value = "HEAD"
@@ -62,37 +86,150 @@ public abstract class LevelRendererMixin {
         }
     }
 
-    //use the "rain level" to hide the sun and moon in the cave biomes.
-    @Redirect(method = "Lnet/minecraft/client/renderer/LevelRenderer;renderSky(Lcom/mojang/blaze3d/vertex/PoseStack;Lorg/joml/Matrix4f;FLnet/minecraft/client/Camera;ZLjava/lang/Runnable;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;getRainLevel(F)F"))
-    private float ac_getRainLevel(ClientLevel instance, float f) {
-        float rainLevel = instance.getRainLevel(f);
-        if (AlexsCaves.CLIENT_CONFIG.biomeSkyOverrides.get()) {
-            rainLevel = Math.max(ACBiomeRegistry.calculateBiomeSkyOverride(Minecraft.getInstance().cameraEntity), rainLevel);
+    @Inject(method = "Lnet/minecraft/client/renderer/LevelRenderer;renderSky(Lcom/mojang/blaze3d/vertex/PoseStack;Lorg/joml/Matrix4f;FLnet/minecraft/client/Camera;ZLjava/lang/Runnable;)V",
+            at = @At(
+                    value = "HEAD"
+            ),
+            allow = 1)
+    //have to completely override this method for compatibility reasons
+    private void ac_renderSky(PoseStack poseStack, Matrix4f matrix4f2, float partialTick, Camera camera, boolean foggy, Runnable runnable, CallbackInfo ci) {
+        ci.cancel();
+        if (level.effects().renderSky(level, ticks, partialTick, poseStack, camera, matrix4f2, foggy, runnable))
+            return;
+        runnable.run();
+        if (!foggy) {
+            FogType fogtype = camera.getFluidInCamera();
+            if (fogtype != FogType.POWDER_SNOW && fogtype != FogType.LAVA && !this.doesMobEffectBlockSky(camera)) {
+                if (this.minecraft.level.effects().skyType() == DimensionSpecialEffects.SkyType.END) {
+                    this.renderEndSky(poseStack);
+                } else if (this.minecraft.level.effects().skyType() == DimensionSpecialEffects.SkyType.NORMAL) {
+                    Vec3 vec3 = this.level.getSkyColor(this.minecraft.gameRenderer.getMainCamera().getPosition(), partialTick);
+                    float f = (float)vec3.x;
+                    float f1 = (float)vec3.y;
+                    float f2 = (float)vec3.z;
+                    FogRenderer.levelFogColor();
+                    BufferBuilder bufferbuilder = Tesselator.getInstance().getBuilder();
+                    RenderSystem.depthMask(false);
+                    RenderSystem.setShaderColor(f, f1, f2, 1.0F);
+                    ShaderInstance shaderinstance = RenderSystem.getShader();
+                    this.skyBuffer.bind();
+                    this.skyBuffer.drawWithShader(poseStack.last().pose(), matrix4f2, shaderinstance);
+                    VertexBuffer.unbind();
+                    RenderSystem.enableBlend();
+                    float[] afloat = this.level.effects().getSunriseColor(this.level.getTimeOfDay(partialTick), partialTick);
+
+                    // AC CODE START
+                    //remove sunrises inside cave biomes.
+                    if (afloat != null && afloat.length >= 4 && AlexsCaves.CLIENT_CONFIG.biomeSkyOverrides.get()) {
+                        float override = ACBiomeRegistry.calculateBiomeSkyOverride(Minecraft.getInstance().cameraEntity);
+                        afloat[3] = afloat[3] * (1F - override);
+                    }
+                    // AC CODE END
+
+                    if (afloat != null) {
+                        RenderSystem.setShader(GameRenderer::getPositionColorShader);
+                        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+                        poseStack.pushPose();
+                        poseStack.mulPose(Axis.XP.rotationDegrees(90.0F));
+                        float f3 = Mth.sin(this.level.getSunAngle(partialTick)) < 0.0F ? 180.0F : 0.0F;
+                        poseStack.mulPose(Axis.ZP.rotationDegrees(f3));
+                        poseStack.mulPose(Axis.ZP.rotationDegrees(90.0F));
+                        float f4 = afloat[0];
+                        float f5 = afloat[1];
+                        float f6 = afloat[2];
+                        Matrix4f matrix4f = poseStack.last().pose();
+                        bufferbuilder.begin(VertexFormat.Mode.TRIANGLE_FAN, DefaultVertexFormat.POSITION_COLOR);
+                        bufferbuilder.vertex(matrix4f, 0.0F, 100.0F, 0.0F).color(f4, f5, f6, afloat[3]).endVertex();
+                        int i = 16;
+
+                        for(int j = 0; j <= 16; ++j) {
+                            float f7 = (float)j * ((float)Math.PI * 2F) / 16.0F;
+                            float f8 = Mth.sin(f7);
+                            float f9 = Mth.cos(f7);
+                            bufferbuilder.vertex(matrix4f, f8 * 120.0F, f9 * 120.0F, -f9 * 40.0F * afloat[3]).color(afloat[0], afloat[1], afloat[2], 0.0F).endVertex();
+                        }
+
+                        BufferUploader.drawWithShader(bufferbuilder.end());
+                        poseStack.popPose();
+                    }
+
+                    RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+                    poseStack.pushPose();
+                    // AC CODE START
+                    //use the "rain level" to hide the sun and moon in the cave biomes.
+                    float rainLevel = this.level.getRainLevel(partialTick);
+
+                    if (AlexsCaves.CLIENT_CONFIG.biomeSkyOverrides.get()) {
+                        rainLevel = Math.max(ACBiomeRegistry.calculateBiomeSkyOverride(Minecraft.getInstance().cameraEntity), rainLevel);
+                    }
+                    float f11 = 1.0F - rainLevel;
+                    // AC CODE END
+                    RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, f11);
+                    poseStack.mulPose(Axis.YP.rotationDegrees(-90.0F));
+                    poseStack.mulPose(Axis.XP.rotationDegrees(this.level.getTimeOfDay(partialTick) * 360.0F));
+                    Matrix4f matrix4f1 = poseStack.last().pose();
+                    float f12 = 30.0F;
+                    RenderSystem.setShader(GameRenderer::getPositionTexShader);
+                    RenderSystem.setShaderTexture(0, SUN_LOCATION);
+                    bufferbuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
+                    bufferbuilder.vertex(matrix4f1, -f12, 100.0F, -f12).uv(0.0F, 0.0F).endVertex();
+                    bufferbuilder.vertex(matrix4f1, f12, 100.0F, -f12).uv(1.0F, 0.0F).endVertex();
+                    bufferbuilder.vertex(matrix4f1, f12, 100.0F, f12).uv(1.0F, 1.0F).endVertex();
+                    bufferbuilder.vertex(matrix4f1, -f12, 100.0F, f12).uv(0.0F, 1.0F).endVertex();
+                    BufferUploader.drawWithShader(bufferbuilder.end());
+                    f12 = 20.0F;
+                    RenderSystem.setShaderTexture(0, MOON_LOCATION);
+                    int k = this.level.getMoonPhase();
+                    int l = k % 4;
+                    int i1 = k / 4 % 2;
+                    float f13 = (float)(l + 0) / 4.0F;
+                    float f14 = (float)(i1 + 0) / 2.0F;
+                    float f15 = (float)(l + 1) / 4.0F;
+                    float f16 = (float)(i1 + 1) / 2.0F;
+                    bufferbuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
+                    bufferbuilder.vertex(matrix4f1, -f12, -100.0F, f12).uv(f15, f16).endVertex();
+                    bufferbuilder.vertex(matrix4f1, f12, -100.0F, f12).uv(f13, f16).endVertex();
+                    bufferbuilder.vertex(matrix4f1, f12, -100.0F, -f12).uv(f13, f14).endVertex();
+                    bufferbuilder.vertex(matrix4f1, -f12, -100.0F, -f12).uv(f15, f14).endVertex();
+                    BufferUploader.drawWithShader(bufferbuilder.end());
+                    float f10 = this.level.getStarBrightness(partialTick) * f11;
+                    if (f10 > 0.0F) {
+                        RenderSystem.setShaderColor(f10, f10, f10, f10);
+                        FogRenderer.setupNoFog();
+                        this.starBuffer.bind();
+                        this.starBuffer.drawWithShader(poseStack.last().pose(), matrix4f2, GameRenderer.getPositionShader());
+                        VertexBuffer.unbind();
+                        runnable.run();
+                    }
+
+                    RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+                    RenderSystem.disableBlend();
+                    RenderSystem.defaultBlendFunc();
+                    poseStack.popPose();
+                    RenderSystem.setShaderColor(0.0F, 0.0F, 0.0F, 1.0F);
+                    double horizonHeight = this.level.getLevelData().getHorizonHeight(this.level);
+                    // AC CODE START
+                    //use the "horizon height" to hide the ugly black line in the distance of cave biomes.
+                    if (AlexsCaves.CLIENT_CONFIG.biomeSkyOverrides.get()) {
+                        horizonHeight = -this.level.getMaxBuildHeight();
+                    }
+                    double d0 = this.minecraft.player.getEyePosition(partialTick).y - horizonHeight;
+                    // AC CODE END
+                    if (d0 < 0.0D) {
+                        poseStack.pushPose();
+                        poseStack.translate(0.0F, 12.0F, 0.0F);
+                        this.darkBuffer.bind();
+                        this.darkBuffer.drawWithShader(poseStack.last().pose(), matrix4f2, shaderinstance);
+                        VertexBuffer.unbind();
+                        poseStack.popPose();
+                    }
+
+                    RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+                    RenderSystem.depthMask(true);
+                }
+            }
         }
-        return rainLevel;
     }
-
-
-    //use the "horizon height" to hide the ugly black line in the distance of cave biomes.
-    @Redirect(method = "Lnet/minecraft/client/renderer/LevelRenderer;renderSky(Lcom/mojang/blaze3d/vertex/PoseStack;Lorg/joml/Matrix4f;FLnet/minecraft/client/Camera;ZLjava/lang/Runnable;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel$ClientLevelData;getHorizonHeight(Lnet/minecraft/world/level/LevelHeightAccessor;)D"))
-    private double ac_getHorizonHeight(ClientLevel.ClientLevelData instance, LevelHeightAccessor levelHeightAccessor) {
-        if (AlexsCaves.CLIENT_CONFIG.biomeSkyOverrides.get()) {
-            return -levelHeightAccessor.getMaxBuildHeight();
-        }
-        return instance.getHorizonHeight(levelHeightAccessor);
-    }
-
-    //remove sunrises inside cave biomes.
-    @Redirect(method = "Lnet/minecraft/client/renderer/LevelRenderer;renderSky(Lcom/mojang/blaze3d/vertex/PoseStack;Lorg/joml/Matrix4f;FLnet/minecraft/client/Camera;ZLjava/lang/Runnable;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/DimensionSpecialEffects;getSunriseColor(FF)[F"))
-    private float[] ac_getSunriseColor(DimensionSpecialEffects instance, float f, float f1) {
-        float[] sunriseColors = instance.getSunriseColor(f, f1);
-        if (sunriseColors != null && sunriseColors.length >= 4 && AlexsCaves.CLIENT_CONFIG.biomeSkyOverrides.get()) {
-            float override = ACBiomeRegistry.calculateBiomeSkyOverride(Minecraft.getInstance().cameraEntity);
-            sunriseColors[3] = sunriseColors[3] * (1F - override);
-        }
-        return sunriseColors;
-    }
-
     @Inject(method = "Lnet/minecraft/client/renderer/LevelRenderer;getLightColor(Lnet/minecraft/world/level/BlockAndTintGetter;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;)I",
             at = @At("HEAD"),
             cancellable = true)
