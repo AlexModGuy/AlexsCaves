5b07f21999289a601d1ac1be0979af5faffe4b98
Alexthe668 <alex.rowlands@cox.net>
2023-10-19 08:58:09 -0700

    fix #6

--- src/main/java/com/github/alexmodguy/alexscaves/mixin/client/MinecraftMixin.java
+++ src/main/java/com/github/alexmodguy/alexscaves/mixin/client/MinecraftMixin.java
@@ -17,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import javax.annotation.Nullable;
 
-@Mixin(Minecraft.class)
+@Mixin(value = Minecraft.class, priority = -100)
 public abstract class MinecraftMixin {
 
     @Shadow
