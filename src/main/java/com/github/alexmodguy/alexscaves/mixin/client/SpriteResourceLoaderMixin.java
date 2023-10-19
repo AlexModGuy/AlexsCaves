9f0b295cd0a61446ec00c9ca7ee617dfe7794ebc
KreloX <krelox@seznam.cz>
2023-10-19 19:29:02 +0200

    support non-vanilla trim materials

---
+++ src/main/java/com/github/alexmodguy/alexscaves/mixin/client/SpriteResourceLoaderMixin.java
@@ -0,0 +1,51 @@
+package com.github.alexmodguy.alexscaves.mixin.client;
+
+import com.github.alexmodguy.alexscaves.AlexsCaves;
+import com.google.common.collect.ImmutableList;
+import net.minecraft.client.renderer.texture.atlas.SpriteResourceLoader;
+import net.minecraft.client.renderer.texture.atlas.SpriteSource;
+import net.minecraft.client.renderer.texture.atlas.sources.PalettedPermutations;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.ResourceManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+
+@Mixin(SpriteResourceLoader.class)
+public abstract class SpriteResourceLoaderMixin {
+
+    @Inject(method = "load",
+            at = @At("RETURN"))
+    private static void ac_load(ResourceManager resourceManager, ResourceLocation location, CallbackInfoReturnable<SpriteResourceLoader> cir) {
+        if (location.getPath().equals("armor_trims")) {
+            SpriteResourceLoader ret = cir.getReturnValue();
+            for (SpriteSource source : ((SpriteResourceLoaderMixin) (Object) ret).getSources()) {
+                if (source instanceof PalettedPermutationsAccessor permutations && permutations.getPaletteKey().getPath().equals("trims/color_palettes/trim_palette")) {
+                    ResourceLocation trimLocation = new ResourceLocation(AlexsCaves.MODID, "trims/models/armor/polarity");
+                    ResourceLocation leggingsTrimLocation = new ResourceLocation(AlexsCaves.MODID, "trims/models/armor/polarity").withSuffix("_leggings");
+                    permutations.setTextures(ImmutableList.<ResourceLocation>builder().addAll(permutations.getTextures()).add(trimLocation, leggingsTrimLocation).build());
+                }
+            }
+        }
+    }
+
+    @Accessor("sources")
+    abstract List<SpriteSource> getSources();
+
+    @Mixin(PalettedPermutations.class)
+    private interface PalettedPermutationsAccessor {
+
+        @Accessor
+        List<ResourceLocation> getTextures();
+
+        @Accessor("textures")
+        void setTextures(List<ResourceLocation> value);
+
+        @Accessor
+        ResourceLocation getPaletteKey();
+    }
+}
