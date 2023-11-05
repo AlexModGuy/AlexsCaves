fbc791ae5974a49b8041a68308070ce9c1b33b01
Alexthe668 <alex.rowlands@cox.net>
2023-11-05 10:21:51 -0800

    fix #379 #378 #191

--- src/main/java/com/github/alexmodguy/alexscaves/server/block/UraniumRodBlock.java
+++ src/main/java/com/github/alexmodguy/alexscaves/server/block/UraniumRodBlock.java
@@ -1,23 +1,41 @@
 package com.github.alexmodguy.alexscaves.server.block;
 
 import com.github.alexmodguy.alexscaves.client.particle.ACParticleRegistry;
+import com.github.alexmodguy.alexscaves.server.block.fluid.ACFluidRegistry;
+import com.github.alexmodguy.alexscaves.server.item.ACItemRegistry;
 import com.github.alexmodguy.alexscaves.server.misc.ACMath;
 import com.github.alexmodguy.alexscaves.server.misc.ACSoundRegistry;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.RotatedPillarBlock;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class UraniumRodBlock extends RotatedPillarBlock {
+import javax.annotation.Nullable;
+import java.util.Optional;
+
+public class UraniumRodBlock extends RotatedPillarBlock implements SimpleWaterloggedBlock  {
 
     private static final VoxelShape SHAPE_X = ACMath.buildShape(
             Block.box(2, 6, 6, 14, 10, 10),
@@ -37,9 +55,11 @@ public class UraniumRodBlock extends RotatedPillarBlock {
             Block.box(5, 5, 0, 11, 11, 2)
     );
 
+    public static final IntegerProperty LIQUID_LOGGED = IntegerProperty.create("liquid_logged", 0, 2);
 
     public UraniumRodBlock() {
         super(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_LIGHT_GREEN).strength(1.5F).lightLevel((state -> 9)).emissiveRendering((state, level, pos) -> true).sound(ACSoundTypes.URANIUM));
+        this.registerDefaultState(this.defaultBlockState().setValue(LIQUID_LOGGED, 0).setValue(AXIS, Direction.Axis.Y));
     }
 
     public VoxelShape getShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext context) {
@@ -63,5 +83,85 @@ public class UraniumRodBlock extends RotatedPillarBlock {
             Vec3 center = Vec3.upFromBottomCenterOf(pos, 0.5F);
             level.addParticle(ACParticleRegistry.PROTON.get(), center.x, center.y, center.z, center.x, center.y, center.z);
         }
+    }
+
+    public BlockState updateShape(BlockState state, Direction direction, BlockState state1, LevelAccessor levelAccessor, BlockPos blockPos, BlockPos blockPos1) {
+        int liquidType = state.getValue(LIQUID_LOGGED);
+        if (liquidType == 1) {
+            levelAccessor.scheduleTick(blockPos, Fluids.WATER, Fluids.WATER.getTickDelay(levelAccessor));
+        } else if (liquidType == 2) {
+            levelAccessor.scheduleTick(blockPos, ACFluidRegistry.ACID_FLUID_SOURCE.get(), ACFluidRegistry.ACID_FLUID_SOURCE.get().getTickDelay(levelAccessor));
+        }
+        if (!levelAccessor.isClientSide()) {
+            levelAccessor.scheduleTick(blockPos, this, 1);
+        }
+
+        return super.updateShape(state, direction, state1, levelAccessor, blockPos, blockPos1);
+    }
+
+    @Nullable
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        LevelAccessor levelaccessor = context.getLevel();
+        BlockPos blockpos = context.getClickedPos();
+        return super.getStateForPlacement(context).setValue(LIQUID_LOGGED, getLiquidType(levelaccessor.getFluidState(blockpos)));
+    }
+
+    public boolean canPlaceLiquid(BlockGetter getter, BlockPos blockPos, BlockState blockState, Fluid fluid) {
+        return fluid == Fluids.WATER || fluid.getFluidType() == ACFluidRegistry.ACID_FLUID_TYPE.get();
+    }
+
+    public boolean placeLiquid(LevelAccessor levelAccessor, BlockPos pos, BlockState blockState, FluidState fluidState) {
+        int liquidType = blockState.getValue(LIQUID_LOGGED);
+        if (liquidType == 0) {
+            if (!levelAccessor.isClientSide()) {
+                if (fluidState.getType() == Fluids.WATER) {
+                    levelAccessor.setBlock(pos, blockState.setValue(LIQUID_LOGGED, 1), 3);
+                } else if (fluidState.getFluidType() == ACFluidRegistry.ACID_FLUID_TYPE.get()) {
+                    levelAccessor.setBlock(pos, blockState.setValue(LIQUID_LOGGED, 2), 3);
+                }
+                levelAccessor.scheduleTick(pos, fluidState.getType(), fluidState.getType().getTickDelay(levelAccessor));
+            }
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public ItemStack pickupBlock(LevelAccessor levelAccessor, BlockPos blockPos, BlockState state) {
+        int liquidType = state.getValue(LIQUID_LOGGED);
+        if (liquidType > 0) {
+            levelAccessor.setBlock(blockPos, state.setValue(LIQUID_LOGGED, 0), 3);
+            if (!state.canSurvive(levelAccessor, blockPos)) {
+                levelAccessor.destroyBlock(blockPos, true);
+            }
+            return new ItemStack(liquidType == 1 ? Items.WATER_BUCKET : ACItemRegistry.ACID_BUCKET.get());
+        } else {
+            return ItemStack.EMPTY;
+        }
+    }
+
+    @Override
+    public Optional<SoundEvent> getPickupSound() {
+        return Fluids.WATER.getPickupSound();
+    }
+
+
+    public FluidState getFluidState(BlockState state) {
+        int liquidType = state.getValue(LIQUID_LOGGED);
+        return liquidType == 1 ? Fluids.WATER.getSource(false) : liquidType == 2 ? ACFluidRegistry.ACID_FLUID_SOURCE.get().getSource(false) : super.getFluidState(state);
+    }
+
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> blockStateBuilder) {
+        blockStateBuilder.add(LIQUID_LOGGED, AXIS);
+    }
+
+    public static int getLiquidType(FluidState fluidState) {
+        if (fluidState.getType() == Fluids.WATER) {
+            return 1;
+        } else if (fluidState.getFluidType() == ACFluidRegistry.ACID_FLUID_TYPE.get()) {
+            return 2;
+        }
+        return 0;
     }
 }
