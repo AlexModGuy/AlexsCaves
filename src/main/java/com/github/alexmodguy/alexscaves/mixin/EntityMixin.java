b2624c785ef8a583f78fb320791d102d88e84b42
Eren Korkmaz <erengkorkmaz@gmail.com>
2025-08-23 01:17:01 +0300

    fixed illagers not striking back

--- src/main/java/com/github/alexmodguy/alexscaves/mixin/EntityMixin.java
+++ src/main/java/com/github/alexmodguy/alexscaves/mixin/EntityMixin.java
@@ -22,7 +22,6 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
